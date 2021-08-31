fab7fe3a8e06a301071bebaa387d4ef02ea45121
marcxz <marcxz@gmail.com>
2021-08-31 01:18:41 -0500

    Remove commented code from testing

--- test/test.ts
+++ test/test.ts
@@ -26,31 +26,3 @@ describe('QueryGeocoding()', async function () {
 
 
 });
-  /*
-  describe('QueryGeocoding()', async function () {
-    it('Test login With noadmin Credentials', async function () {
-  
-      expect(true).to.be.equal(await (await loginFunction("noadmin", "noPow3r")).valid);
-    });
-  });
-  
-  describe('loginFunction()', async function () {
-    it('Test login With bob Credentials', async function () {
-  
-      expect(true).to.be.equal(await (await loginFunction("bob", "thisIsNotAPasswordBob")).valid);
-    });
-  });
-  
-  describe('loginFunction()', async function () {
-    it('Test login With BAD Credentials', async function () {
-  
-      expect(false).to.be.equal(await (await loginFunction("miguel", "itisntpass")).valid);
-    });
-  });
-  
-  describe('protectFunction()', function () {
-    it('Test protected', function () {
-  
-      expect("You are under protected data").to.be.equal(protectFunction("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI"));
-    }); 
-  });*/
